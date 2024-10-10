3c1731f378721568e1592c5de4354641ca782c1a
zhigunovsemyon <zhigunovsemen@gmail.com>
2024-10-10 13:13:07 +0300

    Навалил много. Извините

--- src/main.cc
+++ src/main.cc
@@ -16,8 +16,9 @@
 также их массивов, при передаче объекта в виде форм. пар-ра по умолчанию и
 возврате его по зн. в качестве результата.*/
 
-#include <cstddef>
-#include <iostream>
+#include <climits> /*INT_MAX INT_MIN*/
+#include <ctime> /*time()*/
+#include <iostream> /*cout*/
 
 // Вывод трассировки
 std::ostream &trace = std::cout;
@@ -45,15 +46,19 @@ class Matrix {
         ::trace << "Адрес созданного объекта: " << this << '\n';
         // Задание числа строк
         this->line_count = size;
-        // Выделение памяти под вектор указателей
-        ptr = new int *[size];
+        // Выделение памяти под вектор указателей и размеров
+        this->ptr = new int *[size];
+        this->column_count = new size_t[size];
         ::trace << "Адрес созданной памяти: " << this->ptr << '\n';
 
         for (std::size_t i = 0; i < size; i++) {
+            this->column_count[i] = size;
             this->ptr[i] = new int[size];
             ::trace << "Выделение памяти под строку " << i
                     << " по адресу: " << this->ptr[i] << '\n';
         }
+
+		this->Zero();
     }
 
     // Деструктор
@@ -71,13 +76,53 @@ class Matrix {
                 << this->ptr                  //
                 << " В объекте под адресом: " << this << '\n';
         delete[] this->ptr;
+        delete[] this->column_count;
     }
+
+	// Вывод матрицы в stdout
+    void Print() const {
+        for (std::size_t i = 0; i < this->line_count; i++) {
+            for (std::size_t j = 0; j < this->column_count[i]; j++)
+                std::cout << ptr[i][j] << ' ';
+            
+            std::cout << '\n';
+        }
+    }
+
+	void Randomise(int const max = INT_MAX, int const min = INT_MIN) {
+        for (std::size_t i = 0; i < this->line_count; i++) {
+            for (std::size_t j = 0; j < this->column_count[i]; j++)
+				this->ptr[i][j] = min + std::rand() % (max + 1 - min);
+		}
+	}
+
+	void Zero(){
+        for (std::size_t i = 0; i < this->line_count; i++) {
+            for (std::size_t j = 0; j < this->column_count[i]; j++)
+                ptr[i][j] = 0;
+        }
+	}
 };
 
 int main(void) {
+	std::srand(static_cast<unsigned int>(std::time(NULL)));
     Matrix stackm(2);
     Matrix &heapm = *new Matrix;
-    std::cout << "Hello World!\n";
+	std::cout << "Матрица 1: \n";
+	stackm.Print();
+	std::cout << "Матрица 2: \n";
+	heapm.Print();
+
+	std::cout << "\nРандомизация:\n";
+	stackm.Randomise(10, 0);
+	heapm.Randomise();
+
+	std::cout << "Матрица 1: \n";
+	stackm.Print();
+	std::cout << "Матрица 2: \n";
+	heapm.Print();
+
+	std::cout << '\n';
     delete &heapm;
     return EXIT_SUCCESS;
 }
